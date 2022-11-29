28735cebc314f0b080a41350adae8612b6fc7814
Nir Tzachar <nir.tzachar@gmail.com>
2022-11-29 10:43:14 +0200

    improve compare function

--- lua/cmp_fuzzy_path/compare.lua
+++ lua/cmp_fuzzy_path/compare.lua
@@ -1,6 +1,10 @@
 return function(entry1, entry2)
   if entry1.source.name == 'fuzzy_path' and entry2.source.name == 'fuzzy_path' then
-    return (entry1.completion_item.data.score > entry2.completion_item.data.score)
+    if entry1.completion_item.data.score == entry2.completion_item.data.score then
+      return (#entry1.completion_item.data.path < #entry2.completion_item.data.path)
+    else
+      return (entry1.completion_item.data.score > entry2.completion_item.data.score)
+    end
   else
     return nil
   end
