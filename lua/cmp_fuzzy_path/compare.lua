b0eec44c4591e4937e5ddd367f0440cc90112426
Nir Tzachar <nir.tzachar@gmail.com>
2023-02-19 21:13:21 +0200

    chore: add autogenerate doc plus stylua

--- lua/cmp_fuzzy_path/compare.lua
+++ lua/cmp_fuzzy_path/compare.lua
@@ -1,7 +1,7 @@
 return function(entry1, entry2)
   if entry1.source.name == 'fuzzy_path' and entry2.source.name == 'fuzzy_path' then
     if entry1.completion_item.data.score == entry2.completion_item.data.score then
-      return (#entry1.completion_item.data.path < #entry2.completion_item.data.path)
+      return (#(entry1.completion_item.data.path or '') < #(entry2.completion_item.data.path or ''))
     else
       return (entry1.completion_item.data.score > entry2.completion_item.data.score)
     end
